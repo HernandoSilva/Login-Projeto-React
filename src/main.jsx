d20ede3dbcb3c86c062d3cad10915b63d267ddf6
HernandoSilva <hernando_silva2004@outlook.com>
2023-11-28 08:00:40 -0300

    Projeto de login em React

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import LoginSignup from './Components/LoginSignup/LoginSignup'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <LoginSignup />
+  </React.StrictMode>
+)
